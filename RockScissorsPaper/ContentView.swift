6260d6ca4357373ecefe8251959a1ae471b9491d
iiiivaska <iiiivaska@yandex.ru>
2021-09-26 20:32:59 +0300

    Initial Commit

---
+++ RockScissorsPaper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockScissorsPaper
+//
+//  Created by Василий Буланов on 9/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockScissorsPaper/RockScissorsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockScissorsPaperApp.swift
+//  RockScissorsPaper
+//
+//  Created by Василий Буланов on 9/26/21.
+//
+
+import SwiftUI
+
+@main
+struct RockScissorsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
