53cdaedccb6b04e48ee334b732504bd873316199
iiiivaska <iiiivaska@yandex.ru>
2021-09-26 21:25:53 +0300

    Create core of game: Each turn of the game the app will randomly pick either rock, paper, or scissors. Each turn the app will either prompt the player to win or lose. The player must then tap the correct move to win or lose the game. If they are correct they score a point; otherwise they lose a point. The game ends after 10 questions, at which point their score is shown.

--- RockScissorsPaper/ContentView.swift
+++ RockScissorsPaper/ContentView.swift
@@ -8,9 +8,104 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var score = 0
+    @State private var appSelect = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
+    @State private var gamesCount = 0
+    @State private var endGame = false
+    var moves = ["Rock", "Paper", "Scissors"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(score)")
+            Text("App move: \(moves[appSelect])")
+            Text("Player should: \(shouldWin ? "Win": "Lose")")
+            HStack {
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.buttonTapped(number)
+                    }) {
+                        Text("\(moves[number])")
+                    }
+                }
+            }
+        }
+        .alert(isPresented: $endGame) {
+            Alert(title: Text("End of Round"), message: Text("Your Score: \(score)"), dismissButton: .default(Text("OK")) {
+                score = 0
+                gamesCount = 0
+                appSelect = Int.random(in: 0...2)
+                shouldWin = Bool.random()
+            })
+        }
+    }
+    
+    func buttonTapped(_ number: Int) {
+        if (shouldWin)
+        {
+            switch number{
+            case 0:
+                if (appSelect == 2)
+                {
+                    score += 1
+                } else {
+                    self.score = (number - appSelect == 0) ? score : score - 1
+                }
+                break
+            case 1:
+                if (appSelect == 0)
+                {
+                    score += 1
+                } else {
+                    self.score = (number - appSelect == 0) ? score : score - 1
+                }
+                break
+            case 2:
+                if (appSelect == 1)
+                {
+                    score += 1
+                } else {
+                    self.score = (number - appSelect == 0) ? score : score - 1
+                }
+                break
+            default:
+                break
+            }
+        } else {
+            switch number{
+            case 0:
+                if (appSelect == 2)
+                {
+                    score -= 1
+                } else {
+                    self.score = (number - appSelect == 0) ? score : score + 1
+                }
+                break
+            case 1:
+                if (appSelect == 0)
+                {
+                    score -= 1
+                } else {
+                    self.score = (number - appSelect == 0) ? score : score + 1
+                }
+                break
+            case 2:
+                if (appSelect == 1)
+                {
+                    score -= 1
+                } else {
+                    self.score = (number - appSelect == 0) ? score : score + 1
+                }
+                break
+            default:
+                break
+            }
+        }
+        appSelect = Int.random(in: 0...2)
+        shouldWin = Bool.random()
+        gamesCount += 1
+        if (gamesCount == 9) {
+            endGame = true
+        }
     }
 }
 
